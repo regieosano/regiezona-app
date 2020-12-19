2215461ebf133ae6f8939c05642622db151dad61
regieosano <osanoreginald@gmail.com>
2020-12-19 22:34:53 +0800

    Product and Rating components created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,32 @@
-
-import './App.scss';
+import "./App.scss";
+import Product from "./components/Product";
 import data from "./data";
 
 function App() {
   return (
-     <>
-        <div className="grid-container">
-            <header className="row">
-               <div>
-                   <a href="/" className="brand">regiezona</a>
-               </div>
-               <div>
-                   <a href="/cart">Cart</a>
-                   <a href="/signin">Sign In</a>
-               </div>
-            </header>
-            <main>
-               
-               <div className="row center">
-               {
-                    data.products.map((p) => (
-                     <div key={p._id} className="card">
-                       <a href={`/product/${p._id}`}>
-                          <img className="medium" src={p.image} alt={p.name}></img>
-                       </a>
-                      <div className="card-body">
-                        <a  href={`/product/${p._id}`}>
-                            <h2>{p.name}</h2>
-                        </a>
-                        <div className="rating">
-                           <span><i className="fa fa-star"></i></span>
-                           <span><i className="fa fa-star"></i></span>
-                           <span><i className="fa fa-star"></i></span>
-                           <span><i className="fa fa-star"></i></span>
-                           <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">
-                            ${p.price}
-                        </div>
-                      </div>
-                    </div>
-                    ))
-               }
-                 
-                 
-               </div>
-            </main>
-            <footer className="row center">
-                All right reserved
-            </footer>
-        </div>
-     </>
+    <>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a href="/" className="brand">
+              regiezona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.scss';
+import "./index.scss";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
